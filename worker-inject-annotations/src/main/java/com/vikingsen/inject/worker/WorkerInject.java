1c6f3b5941a91b5818d50ad005580821a0be7536
Jordan Hansen <jordanihansen10@gmail.com>
2019-10-24 05:53:51 -0600

    Change the retention policy of @WorkerInject and @WorkerModule Aggregating annotation processors require class or runtime retention Jordan Hansen 10/24/19 5:34 AM

--- worker-inject-annotations/src/main/java/com/vikingsen/inject/worker/WorkerInject.java
+++ worker-inject-annotations/src/main/java/com/vikingsen/inject/worker/WorkerInject.java
@@ -4,9 +4,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
-@Retention(SOURCE)
+@Retention(CLASS)
 @Target(CONSTRUCTOR)
 public @interface WorkerInject {
 }
--- worker-inject-annotations/src/main/java/com/vikingsen/inject/worker/WorkerModule.java
+++ worker-inject-annotations/src/main/java/com/vikingsen/inject/worker/WorkerModule.java
@@ -4,9 +4,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 @Target(TYPE)
-@Retention(SOURCE)
+@Retention(CLASS)
 public @interface WorkerModule {
 }
